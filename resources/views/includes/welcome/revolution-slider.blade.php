535b4ddf1854e0ce8b9ae519faab2572ea4ceec0
Norberto Trosman <info@nute.com.ar>
2017-11-28 16:37:54 -0300

    rs fix

--- resources/views/includes/welcome/revolution-slider.blade.php
+++ resources/views/includes/welcome/revolution-slider.blade.php
@@ -89,11 +89,11 @@
 
           <!-- SLIDE  -->
         <li data-index="rs-2802" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default"
-            data-easeout="default" data-masterspeed="default" data-thumb="{{asset('images/images/rs/rs-02.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide"
+            data-easeout="default" data-masterspeed="default" data-thumb="{{asset('images/products/rs/rs-02.jpg')}}" data-rotate="0" data-saveperformance="off" data-title="Slide"
             data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
             data-description="">
           <!-- MAIN IMAGE -->
-          <img src="{{asset('images/images/rs/rs-02.jpg')}}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg">
+          <img src="{{asset('images/products/rs/rs-02.jpg')}}" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg">
           <!-- LAYERS -->
 
           <!-- LAYER NR. 15 -->
